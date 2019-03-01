95407fdcef86dfe752bebddd8bbd0382cea99856
Desmond Lee <d1031lee@Gmail.com>
2019-03-01 15:45:03 -0800

    Source lab 6 commit 1

---
+++ Documents/NetBeansProjects/lab6_Group_Test/src/lab6_group_test/Lab6_Group_Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6_group_test;
+
+/**
+ *
+ * @author d1031
+ */
+public class Lab6_Group_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Desmond
+        System.out.println("Source lab 6"
+                + "");
+    }
+    
+}
